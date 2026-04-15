9498ea2630ee66dfa12a5f130abb9494ba96dab8
Mazen421 <mazenahmed421@yahoo.com>
2026-04-15 19:52:53 +0200

    Fixed a bug where it only detects the website only if refreshed
    Okay so someone told me that this only works if the webpage is refreshed on the episode page, I realized that is because the match had watch at the end, so if you go to the mainpage of crunchyroll and then to an episode tampermonkey doesn't load the script

--- CrunchyRoll Skip Recap Removal-2026-04-15.user.js
+++ CrunchyRoll Skip Recap Removal-2026-04-15.user.js
@@ -4,16 +4,16 @@
 // @version      2026-04-15
 // @description  Removing the skip credit button from the website Crunchyroll
 // @author       You
-// @match        *://www.crunchyroll.com/watch/*
+// @match        *://www.crunchyroll.com/*
 // @icon         https://imgs.search.brave.com/BZ1otucU2u6I0uvHauQTXMmPusA_GLJe4cR0yo4_YmY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL3RodW1icy82/MTM2MjY4NGY1OTY2/OTAwMDQ0Y2JmNzMu/cG5n
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    const observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
+    const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
             if (mutation.addedNodes.length) {
                 const button = document.querySelector('button[aria-label="Skip Recap"]');
                 if (button) {
